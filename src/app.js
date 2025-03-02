38b68c1568d922ba398b5e6f4e43a7814a5574d7
Sergio-Adarve <sergio.adarve@gmail.com>
2025-03-02 16:14:45 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,17 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 function generarExcusa() {
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-  
-      let excusa = who[Math.floor(Math.random() * who.length)] + ' ' +
-                   action[Math.floor(Math.random() * action.length)] + ' ' +
-                   what[Math.floor(Math.random() * what.length)] + ' ' +
-                   when[Math.floor(Math.random() * when.length)];
-  
-      document.getElementById('excuse').innerHTML = excusa;
-  }
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = [
+    'before the class',
+    'when I was sleeping',
+    'while I was exercising',
+    'during my lunch',
+    'while I was praying'
+  ];
+
+  const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+  const excuse = `${getRandomElement(who)} ${getRandomElement(action)} ${getRandomElement(what)} ${getRandomElement(when)}`;
+
+  document.getElementById('excuse').innerText = excuse;
+}
+
   
   window.onload = generarExcusa;
